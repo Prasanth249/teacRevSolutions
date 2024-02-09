a8f204ea3553c0871f61444c4ac18b81eb3c1654
prasanth249 <saiprasanth511@gmail.com>
2024-02-09 11:27:05 +0530

    For clean and test the spring boot code

--- src/main/java/com/example/demo/TestGithubActions.java
+++ src/main/java/com/example/demo/TestGithubActions.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +13,16 @@ public class TestGithubActions {
 	{
 		return "sai";
 	}
+	
+	@PostMapping("/add/{value1}/{value2}")
+	public int sum(@PathVariable int value1, @PathVariable int value2) {
+		return value1+value2;
+	}
+	
+	@GetMapping("/firstNames")
+	public String firstName()
+	{
+		return "phani";
+	}
+	
 }
